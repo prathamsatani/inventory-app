9b0782167c2cc4cbd8da686a79b395880f2ed36c
Pratham Satani <iamprathamsatani2002@gmail.com>
2026-09-09 12:52:29 -0400

    update users table

--- backend/src/models.py
+++ backend/src/models.py
@@ -38,6 +38,8 @@ class Vendor(Base):
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False, index=True)
+    fname: Mapped[str] = mapped_column(String(50), nullable=False)
+    lname: Mapped[str] = mapped_column(String(50), nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     order_history: Mapped[List["OrderHistory"]] = relationship(backref="user", cascade="all, delete-orphan")
